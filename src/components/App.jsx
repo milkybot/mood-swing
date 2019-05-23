91ec1b8f514db38dc96c44b0b312c4d256d0ab82
Marie Dillard <mariedillard@protonmail.com>
2019-05-23 11:30:55 -0700

    builds basic framework.

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./Footer";
+
+function App(){
+    return (
+        <div>
+            title 
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
